12f5673de8f58b83932846aebbf032e62b90ee6e
Flore Meyer-Prevost <floremeyerprevost@gmail.com>
2020-06-03 13:41:32 +0200

    Add comments

--- Assets/Scripts/ControllerBehaviour.cs
+++ Assets/Scripts/ControllerBehaviour.cs
@@ -6,17 +6,18 @@ public class ControllerBehaviour : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpForce;
-    [SerializeField] private LayerMask ground;
+    [SerializeField] private LayerMask ground; // Permet de sélectionner un ou plusieurs layers pour notre sol
 
-    private Rigidbody2D myRB;
+    private Rigidbody2D myRB; // Référence à un rigidbody (le nôtre)
     private Vector2 stickDirection;
-    private Animator myAnimator;
+    private Animator myAnimator; // Référence à un animator (le nôtre)
 
-    private bool isOnGround = false;
+    private bool isOnGround = false; // Permet de vérifier si on a le droit de sauter
     // Start is called before the first frame update
 
     private void OnEnable()
     {
+        //On setup les contrôles avec leurs callbacks respectives
         var controls = new Controls();
         controls.Enable();
         controls.Main.Move.performed += MoveOnperformed;
@@ -24,20 +25,35 @@ public class ControllerBehaviour : MonoBehaviour
         controls.Main.Jump.performed += JumpOnperformed;
     }
 
+    /// <summary>
+    /// Exécutée lorsque le bouton de saut est appuyé
+    /// </summary>
+    /// <param name="obj"></param>
     private void JumpOnperformed(InputAction.CallbackContext obj)
     {
+        // Si isOnGround est vrai
         if (isOnGround)
         {
+            // On saute
             myRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            // Et on désactive la possibilité de sauter à nouveau
             isOnGround = false;
         }
     }
 
+    /// <summary>
+    /// Exécutée lorsque le stick ou les flèches directionnelles sont utilisés
+    /// </summary>
+    /// <param name="obj"></param>
     private void MoveOnperformed(InputAction.CallbackContext obj)
     {
         stickDirection = obj.ReadValue<Vector2>();
     }
 
+    /// <summary>
+    /// Exécutée lorsque l'on relache l'input de déplacement
+    /// </summary>
+    /// <param name="obj"></param>
     private void MoveOncanceled(InputAction.CallbackContext obj)
     {
         stickDirection = Vector2.zero;
@@ -45,6 +61,7 @@ public class ControllerBehaviour : MonoBehaviour
 
     void Start()
     {
+        // On récupère les références à nos composants
         myRB = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
     }
@@ -56,6 +73,7 @@ public class ControllerBehaviour : MonoBehaviour
             x = stickDirection.x,
             y = 0
         };
+        // Déplacement
         if (myRB.velocity.sqrMagnitude < maxSpeed)
         {
             myRB.AddForce(direction * speed);
@@ -71,6 +89,7 @@ public class ControllerBehaviour : MonoBehaviour
            myAnimator.SetBool("IsRunning", false);
         }
         */
+        // Contrôle des variables de l'animator
         var isRunning = isOnGround && Mathf.Abs(myRB.velocity.x) > 0.1f;
         myAnimator.SetBool("IsRunning", isRunning);
         var isAscending = !isOnGround && myRB.velocity.y > 0;
@@ -81,10 +100,12 @@ public class ControllerBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        // Booléen vérifiant si le layer sur lequel on a atteri appartient bien au layerMask "ground"
         var touchGround = ground == (ground | (1 << other.gameObject.layer));
+        // Booléen vérifiant que l'on collisionne avec une surface horizontale
         var touchFromAbove = other.contacts[0].normal == Vector2.up;
         if (touchGround && touchFromAbove)
-        //if (other.gameObject.CompareTag("Ground") == true)
+            //if (other.gameObject.CompareTag("Ground") == true)
         {
             isOnGround = true;
         }
