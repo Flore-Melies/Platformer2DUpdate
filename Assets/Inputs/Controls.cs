a9fdfa3469f1b4927fafeca21bea990e79c43df7
Flore Meyer-Prevost <floremeyerprevost@gmail.com>
2020-06-03 13:29:39 +0200

    First commit

---
+++ Assets/Inputs/Controls.cs
@@ -0,0 +1,227 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Main"",
+            ""id"": ""ee7865d5-24f5-4dbc-bf8b-8ba5bf5254c3"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""9b1ff612-0ce6-4002-99a0-9dc20f6d875c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""4cf6d372-af5c-4d0f-8973-c85dd4eb2f1a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""94973e31-36be-4ee4-b33f-1999d9bcd971"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""b2f57865-9f59-4f03-a776-0f8030abadd1"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""e0266d1c-af9c-4676-9ee9-411bba3e0e3d"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""33c2d4b4-fa9d-4715-a3a3-ac051c13df88"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""020be5b8-ac42-44fb-9cc8-f24f92032e07"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""fa407dc2-be18-4486-a3aa-25ca2ed3f930"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""800f98e5-2bc5-4129-aaf7-c198e576f2bc"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""853e8d35-58ae-4eaf-8cf3-baaab86c6845"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Main
+        m_Main = asset.FindActionMap("Main", throwIfNotFound: true);
+        m_Main_Move = m_Main.FindAction("Move", throwIfNotFound: true);
+        m_Main_Jump = m_Main.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Main
+    private readonly InputActionMap m_Main;
+    private IMainActions m_MainActionsCallbackInterface;
+    private readonly InputAction m_Main_Move;
+    private readonly InputAction m_Main_Jump;
+    public struct MainActions
+    {
+        private @Controls m_Wrapper;
+        public MainActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Main_Move;
+        public InputAction @Jump => m_Wrapper.m_Main_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Main; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MainActions set) { return set.Get(); }
+        public void SetCallbacks(IMainActions instance)
+        {
+            if (m_Wrapper.m_MainActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_MainActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_MainActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_MainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public MainActions @Main => new MainActions(this);
+    public interface IMainActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/ControllerBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask ground;
+
+    private Rigidbody2D myRB;
+    private Vector2 stickDirection;
+    private Animator myAnimator;
+
+    private bool isOnGround = false;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        var controls = new Controls();
+        controls.Enable();
+        controls.Main.Move.performed += MoveOnperformed;
+        controls.Main.Move.canceled += MoveOncanceled;
+        controls.Main.Jump.performed += JumpOnperformed;
+    }
+
+    private void JumpOnperformed(InputAction.CallbackContext obj)
+    {
+        if (isOnGround)
+        {
+            myRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void MoveOnperformed(InputAction.CallbackContext obj)
+    {
+        stickDirection = obj.ReadValue<Vector2>();
+    }
+
+    private void MoveOncanceled(InputAction.CallbackContext obj)
+    {
+        stickDirection = Vector2.zero;
+    }
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        var direction = new Vector2
+        {
+            x = stickDirection.x,
+            y = 0
+        };
+        if (myRB.velocity.sqrMagnitude < maxSpeed)
+        {
+            myRB.AddForce(direction * speed);
+        }
+
+        /*
+        if (Mathf.Abs(myRB.velocity.x) > 0.1f)
+        {
+            myAnimator.SetBool("IsRunning", true);
+        }
+        else
+        {
+           myAnimator.SetBool("IsRunning", false);
+        }
+        */
+        var isRunning = isOnGround && Mathf.Abs(myRB.velocity.x) > 0.1f;
+        myAnimator.SetBool("IsRunning", isRunning);
+        var isAscending = !isOnGround && myRB.velocity.y > 0;
+        myAnimator.SetBool("IsAscending", isAscending);
+        var isDescending = !isOnGround && myRB.velocity.y < 0;
+        myAnimator.SetBool("IsDescending", isDescending);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var touchGround = ground == (ground | (1 << other.gameObject.layer));
+        var touchFromAbove = other.contacts[0].normal == Vector2.up;
+        if (touchGround && touchFromAbove)
+        //if (other.gameObject.CompareTag("Ground") == true)
+        {
+            isOnGround = true;
+        }
+    }
+}
